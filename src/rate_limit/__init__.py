474de62729e013c659c418782f2a8243894d46de
Björn Petersen <git@bjoernpetersen.net>
2023-03-09 11:18:01 +0100

    Remove unused import

--- src/rate_limit/__init__.py
+++ src/rate_limit/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import abc
 from dataclasses import dataclass
 
-from deprecated.classic import deprecated
 from pendulum import DateTime, tz
 from pendulum.tz.timezone import Timezone
 
